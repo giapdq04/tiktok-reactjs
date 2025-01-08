42c2e95188f3a79aa46031d8576d77e5950cdc78
giapdq04 <quanggiap04@gmail.com>
2025-01-08 11:49:44 +0700

    học useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import Content from "./Content";
+
+const tabs = ['posts', 'comments', 'albums']
 
 function App() {
-    const [title, setTitle] = useState('')
+    const [isShow, setIsShow] = useState(false)
     const [posts, setPosts] = useState([])
+    const [type, setType] = useState(tabs[0])
+    const [showGoToTop, setShowGoToTop] = useState(false)
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts/')
-            .then(res => res.json())
-            .then(data => setPosts(data))
-    },[]);
+    // console.log(type)
+
+    // useEffect(() => {
+    //     fetch(`https://jsonplaceholder.typicode.com/${type}`)
+    //         .then(res => res.json())
+    //         .then(data => setPosts(data))
+    // }, [type]);
+    //
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //         console.log(window.scrollY)
+    //         setShowGoToTop(window.scrollY >= 200)
+    //     }
+    //     window.addEventListener('scroll', handleScroll);
+    //
+    //     // hàm dọn dẹp
+    //     return () => {
+    //         window.removeEventListener('scroll', handleScroll)
+    //     }
+    // }, []);
 
 
     return (
         <div className="App">
 
-            <input
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
+            <button onClick={() => setIsShow(!isShow)}>Toggle</button>
+            {isShow && <div>
 
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
+                {/*{*/}
+                {/*    tabs.map((tab, index) =>*/}
+                {/*        <button*/}
+                {/*            key={index}*/}
+                {/*            style={type === tab ? {*/}
+                {/*                color: '#fff',*/}
+                {/*                backgroundColor: '#333'*/}
+                {/*            } : {}}*/}
+                {/*            onClick={() => {*/}
+                {/*                setType(tab)*/}
+                {/*            }}*/}
+                {/*        >{tab}</button>*/}
+                {/*    )*/}
+                {/*}*/}
+
+                {/*<ul>*/}
+                {/*    {posts.map(post => (*/}
+                {/*        <li key={post.id}>{post.title || post.name}</li>*/}
+                {/*    ))}*/}
+                {/*</ul>*/}
+                {/*{showGoToTop && (*/}
+                {/*    <button style={{*/}
+                {/*        position: 'fixed',*/}
+                {/*        right: 20,*/}
+                {/*        bottom: 20*/}
+                {/*    }}>Show</button>*/}
+                {/*)}*/}
+
+                <Content/>
+            </div>}
+
+
         </div>
     );
 }
