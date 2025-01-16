c6b25ab0a67c7dea73551c57b707459657b196b9
giapdq04 <quanggiap04@gmail.com>
2025-01-16 16:14:41 +0700

    Refactor Header component structure and update imports

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header'
 import Sidebar from './Sidebar'
+import Header from '../components/Header'
 
 const DefaultLayout = ({ children }) => {
     return (
